e515fc6ca7920e57828887ec9552a08e7b0ec3b1
Tom Firth <tom.firth@holidayextras.com>
2017-06-15 15:24:26 +0100

    pug and server update

--- index.js
+++ index.js
@@ -1,9 +1,61 @@
 var express = require('express')
 var app = express()
-require('./config/routes')(app)
+var path = require('path')
+var Prismic = require('prismic.io')
+var _ = require('lodash')
+var config = require('./config/default')
+
 app.set('view engine', 'pug')
+app.locals.basedir = path.join(__dirname, '/')
 var port = process.env.port || 8080
 
-app.listen(port, function () {
+var con = {}
+var articles = []
+
+checkPage = (res) => {
+  
+  return many
+}
+
+// organise where content comes from
+getContent = (res) => {
+  var many = checkPage(res)
+  getDocument(res.endpoint)
+  return {
+    pageTitle: res.req.params.endpoint,
+    pageDescription: '',
+    many
+  }
+}
+
+// connect to and get prismic content
+getDocument = (category) => {
+  Prismic.api("https://fomtirth.prismic.io/api")
+  .then((api) => {
+    return api.query(""); // An empty query will return all the documents 
+  })
+  .then((response) => {
+    _.forEach(response.results, (article) => {
+      articles.push({
+        uid: article.uid,
+        title: article.uid,
+        type: article.type,
+        slug: article.slug,
+        data: article.data['blog.richtext']
+      })
+    })
+    console.log('++ articles', articles)
+  }, (err) => {
+    console.log("Something went wrong: ", err)
+  })
+}
+
+app.get(['/', '/:endpoint', '/:endpoint/:uid'], (req, res) => {
+  con = getContent(res)
+  res.render(
+    'page', con)
+})
+
+app.listen(port, () => {
     console.log('all your', port, 'r belong to us')
 })
